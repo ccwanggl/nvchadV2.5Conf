c04dc696da59000a1f2e4eb18093c8a129387d1a
Guoliang WANG <tswanggl@gmail.com>
2024-09-23 20:45:26 +0800

    Add todo commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -114,5 +114,15 @@ return
       'nvim-tree/nvim-web-devicons', -- optional
     },
   opts = {},
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    }
   }
+
 }
