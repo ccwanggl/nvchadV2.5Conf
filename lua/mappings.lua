c382056a257219823106a7e09e56793c5cdd1901
guoliang[MAC-MINI] <guoliang.wang@cust.edu.cn>
2025-05-03 18:39:48 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,8 @@
 require "nvchad.mappings"
 
--- add yours here
-
-local map = vim.keymap.set
-
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map({"i","n","v"}, "<leader>fp", "<cmd> NeoZoomToggle <CR>", {desc = "floating current pane" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set({"i","n","v"}, "<leader>fp", "<cmd> NeoZoomToggle <CR>", {desc = "floating current pane" })
 
 -- Keyboard users
 vim.keymap.set("n", "<C-t>", function()
