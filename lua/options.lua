9fa47047ccb6fd913d07d770ce924a90dbedb359
guoliang (LABTOP) <guoliang.wang@cust.edu.cn>
2025-07-04 14:10:44 +0800

    set expand tab size to 4 space

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,10 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Indenting
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
