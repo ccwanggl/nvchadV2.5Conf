12b3cb44b23318e325faf9fab9dea8acce8d470f
guoliang (LAPTOP) <guoliang.wang@cust.edu.cn>
2025-11-12 17:00:22 +0800

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,3 +1,5 @@
+local conform = require "conform"
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
@@ -6,7 +8,13 @@ local options = {
 
     sh = { "shfmt" },
 
-    python = { "isort", "black" },
+    python = function(bufnr)
+      if conform.get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_fix", "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
 
     rust = { "rustfmt" },
     -- css = { "prettier" },
@@ -16,6 +24,14 @@ local options = {
   formatters = {
     clang_format = {
       prepend_args = { "--style=file", "--fallback-style=LLVM" },
+    },
+
+    ruff_fix = {
+      -- 追加 CLI 参数，例如使用 pyproject.toml：
+      prepend_args = {}, -- { "--config", "pyproject.toml" }
+    },
+    ruff_format = {
+      prepend_args = {}, -- { "--line-length", "100" }
     },
 
     isort = {
@@ -29,11 +45,11 @@ local options = {
     },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = true,
+  },
 }
 
 return options
